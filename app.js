257c645e5a7ca92b2b94462e72799ae76f91970b
Mubaraka Kikonyogo <mubaraka@jentroy.com>
2024-12-14 08:54:01 +0300

    update

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ class SavingsTracker {
 // PWA Service Worker Registration
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/savings-tracker-service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
             .then(registration => {
                 console.log('Service Worker registered successfully:', registration.scope);
             })
